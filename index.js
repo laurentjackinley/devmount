5275c3add5371c9bdbc0864ac382e85a9dab3d62
Jackinley Laurent <laurentjackinley97@gmail.com>
2022-05-25 13:03:35 -0600

    index.js

---
+++ index.js
@@ -0,0 +1,35 @@
+// Jon stats
+let JonSnowAttack = 25;
+let JonSnowHealth = 100;
+let JonSnowDefense = 25;
+//Jamie stas
+let jamieLannisterAttack = 35;
+if (JonSnowAttack > jamieLannisterAttack) {
+    console.log("Jon Snow has better attack than Jon Snow");
+} else if (JonSnowAttack < jamieLannisterAttack) {
+    console.log("Jamie has the better attack");
+} else {
+    console.log("Jamie Lannister has better attack than Jon");
+}
+//Jon snow health = jon health - jamie attack
+if (JonSnowHealth <= jamieLannisterAttack) {
+    console.log("Jon Snow has been slain");
+} else {
+    JonSnowHealth -= jamieLannisterAttack;
+    console.log(`Jon Snow's health is down to ${JonSnowHealth}`);
+}
+//jon pick's up a shield
+JonSnowDefense += 25;
+if (JonSnowHealth <= jamieLannisterAttack - JonSnowDefense) {
+    console.log("Jon Snow has been slain");
+} else {
+    JonSnowHealth -= (jamieLannisterAttack - JonSnowDefense);
+    console.log(`Jon Snow's health is down to ${JonSnowHealth}`);
+}
+// peasan throws Jon a heatlh kit of 50
+let healthkit = 50;
+if ((JonSnowHealth + healthkit) >= 100) {
+    JonSnowHealth = 100;
+} else {
+    JonSnowHealth += healthkit;
+}
